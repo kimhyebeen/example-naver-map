828120e62dc24dd3cc579bea4f3950b192a6bb3e
kimhyebeen <kimhbin@naver.com>
2020-09-10 10:23:15 +0900

    Naver Map 현재 화면 위치에 마커 띄우기 버튼 구현

--- NaverMap/app/src/main/java/com/khb/navermap/MainActivity.kt
+++ NaverMap/app/src/main/java/com/khb/navermap/MainActivity.kt
@@ -8,26 +8,40 @@ import com.naver.maps.geometry.LatLng
 import com.naver.maps.geometry.LatLngBounds
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
+import com.naver.maps.map.NaverMapOptions
 import com.naver.maps.map.NaverMapSdk
 import com.naver.maps.map.NaverMapSdk.NaverCloudPlatformClient
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.overlay.OverlayImage
+import com.naver.maps.map.widget.ZoomControlView
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-//    var naverMap: NaverMap? = null
+    lateinit var mapFragment: MapFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initSetting()
+        initMap()
+
+        button.setOnClickListener {
+            mapFragment.getMapAsync { setMarker(it.cameraPosition.target.latitude, it.cameraPosition.target.longitude, it) }
+        }
+    }
+    private fun initSetting() {
         NaverMapSdk.getInstance(this).client = NaverCloudPlatformClient(StringKeySet.CLIENT_ID)
-
-        val mapFragment = mapFragment as MapFragment?
-
-        // NaverMap 객체 얻어오기
-        mapFragment?.getMapAsync {
-//            naverMap = it
+        supportFragmentManager.let { fm ->
+            mapFragment = fm.findFragmentById(R.id.mapFrame) as MapFragment?
+                ?: MapFragment.newInstance(NaverMapOptions().zoomControlEnabled(false))
+                    .also { map -> fm.beginTransaction().add(R.id.mapFrame, map).commit() }
+        }
+    }
+    private fun initMap() {
+        mapFragment.getMapAsync {
+            val zoomControlView = findViewById<ZoomControlView>(R.id.zoom)
+            zoomControlView.map = it
             it.mapType = NaverMap.MapType.Navi
             it.isNightModeEnabled = true
             it.setLayerGroupEnabled("LAYER_GROUP_BUILDING", true)
